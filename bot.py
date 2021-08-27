7b8cc8e88978fb61cf345ff2871d8cd6c2d1626d
Soebb <87156166+Soebb@users.noreply.github.com>
2021-08-27 14:11:35 +0430

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ import json
 import imdb
 import os
 PORT = int(os.environ.get('PORT', 5000))
-api_key= #Enter your Telegram bot token
+api_key= os.environ.get("BOT_TOKEN")
 ia = imdb.IMDb() 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
@@ -55,7 +55,7 @@ def reply(update, context):
 
 def main():
 
-    updater = Updater("1476373283:AAHrdQE394_J8qd78J974y_AwVdwrsis1r0", use_context=True)
+    updater = Updater(api_key, use_context=True)
     dp = updater.dispatcher
     
     dp.add_handler(CommandHandler("start", start))
